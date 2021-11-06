11511f7aa16e1379a71aac9eebcb263341b0e67c
fanlit <fanlit1998@gmail.com>
2021-11-06 17:29:53 +0800

    Commit: added java docs to orders package

--- OrdersPackage/OrderUI.java
+++ OrdersPackage/OrderUI.java
@@ -7,15 +7,28 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import StaffPackage.*;
 
-
+/**
+ * Main orderUI to recieve user input to do different order functions
+ */
 public class OrderUI {
 
     public OrderUI(){}
 
+    /**
+     * View all existing orders
+     * @param order_Mngr manager responsible for managing orders
+     */
     public static void viewAllOrders(OrderManager order_Mngr){
         order_Mngr.viewAllOrders();
     }
 
+    /**
+     * UI to create new orders
+     * @param cust_Mngr manager responsible for checking customerID
+     * @param staff_Mngr manager responsible for checking StaffID
+     * @param table_Mngr manager responsible for finding available tables
+     * @param order_Mngr manager responsible for storing the new order
+     */
     public static void CreateOrders(CustomerManager cust_Mngr, StaffManager staff_Mngr, TableManager table_Mngr,
                                     OrderManager order_Mngr){
 
@@ -53,7 +66,7 @@ public class OrderUI {
                 }
             }
             catch(InputMismatchException m){
-                System.out.println("ERROR! Please enter the ID of the StaffPackage.Staff!");
+                System.out.println("ERROR! Please enter the ID of the Staff!");
                 userInput.nextLine();
                 continue;
             }
@@ -77,6 +90,10 @@ public class OrderUI {
         order_Mngr.printCustomerAndOrders();
     }
 
+    /**
+     * Method to recieve user input on what order to view
+     * @param order_Mngr manager responsible for finding and displaying the order
+     */
     public static void viewIndividualOrders(OrderManager order_Mngr){
         long orderID;
         Scanner userInput = new Scanner(System.in);
@@ -86,6 +103,12 @@ public class OrderUI {
         order_Mngr.viewOrder(orderID);
     }
 
+    /**
+     * UI to recieve user input on MenuItems/PromoSetMeal to add
+     * @param order_Mngr manager responsible to add the item
+     * @param item_Mngr manager responsible to find the MenuItem to add
+     * @param promo_Mngr manager responsible to find the PromoItem to add
+     */
     public static void AddItemsToOrder(OrderManager order_Mngr, ItemManager item_Mngr, PromoManager promo_Mngr){
         Scanner userInput = new Scanner(System.in);
         long orderID;
@@ -95,7 +118,7 @@ public class OrderUI {
         System.out.println("Please enter the orderID to add");
         orderID = userInput.nextLong();
         if(!order_Mngr.checkOrderExists(orderID)){
-            System.out.println("ERROR! OrdersPackage.Order does not exist!");
+            System.out.println("ERROR! Order does not exist!");
             return;
         }
         do{
@@ -140,6 +163,10 @@ public class OrderUI {
         }while(true);
     }
 
+    /**
+     * UI to recieve user input on which order to remove items from based on orderID
+     * @param order_Mngr manager responsible in removing items from orders
+     */
     public static void removeItemsFromOrder(OrderManager order_Mngr){
         Scanner userInput = new Scanner(System.in);
         long orderID;
@@ -153,6 +180,12 @@ public class OrderUI {
         order_Mngr.removeItemFromOrder(orderID);
     }
 
+    /**
+     * UI to recieve user input on which order to pay
+     * @param order_Mngr manager responsible for order payment
+     * @param invoice_Mngr manager responsible for converting the paid order to invoice
+     * @param table_Mngr manager responsible for unassigning table if applicable
+     */
     public static void settlePayment(OrderManager order_Mngr, InvoiceManager invoice_Mngr, TableManager table_Mngr){
         Scanner userInput = new Scanner(System.in);
         order_Mngr.printCustomerAndOrders();
@@ -161,6 +194,10 @@ public class OrderUI {
         order_Mngr.settlePayment(orderID, invoice_Mngr, table_Mngr);
     }
 
+    /**
+     * UI to recieve user input on which invoice to add based on invoiceID
+     * @param invoice_Mngr
+     */
     public static void printOrderInvoice(InvoiceManager invoice_Mngr){
         Scanner userInput = new Scanner(System.in);
         invoice_Mngr.printInvoiceIDAndDate();
@@ -169,6 +206,10 @@ public class OrderUI {
         invoice_Mngr.printInvoice(invoiceID);
     }
 
+    /**
+     * Invokes invoice_Mngr to print all existing invoices stored in the orders.csv
+     * @param invoice_Mngr manager responsible for retrieving and printing all existing invoices
+     */
     public static void printAllInvoices(InvoiceManager invoice_Mngr){
         invoice_Mngr.printAllInvoices();
     }
